8ebb8b837e1676fa0775da0086c9ea4a36e389aa
SugoiYellow <Jaredlbakker@gmail.com>
2022-01-30 17:03:38 +1300

    Revert "Merge branch 'master' of https://github.com/Haydawg/GameJam2022"
    This reverts commit 754b3fe325c74e818949192ca251fc95ccc3b687, reversing
    changes made to 418257a4de1397fc96776f972e0cd01af2e03402.

--- Assets/Scipts/UiContoller.cs
+++ Assets/Scipts/UiContoller.cs
@@ -7,23 +7,18 @@ using UnityEngine.SceneManagement;
 public class UiContoller : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip[] clips;
-    public GameObject pauseMenu;
+    public AudioClip[] clips; 
     
     // Start is called before the first frame update
     void Start()
     {
-        pauseMenu.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            pauseMenu.SetActive(true);
-            Time.timeScale = 0;
-        }
+
     }
     public void LoadLevel(int index)
     {
