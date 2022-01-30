f173fae87cfce7794877bccf5d1e8915ca986cad
Hayden McGeady <hjm141@uclive.ac.nz>
2022-01-30 16:10:12 +1300

    pause menu

--- Assets/Scipts/UiContoller.cs
+++ Assets/Scipts/UiContoller.cs
@@ -7,18 +7,23 @@ using UnityEngine.SceneManagement;
 public class UiContoller : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip[] clips; 
+    public AudioClip[] clips;
+    public GameObject pauseMenu;
     
     // Start is called before the first frame update
     void Start()
     {
-
+        pauseMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
     public void LoadLevel(int index)
     {
